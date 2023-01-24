69fe8d0942a0c3ab9052e68228d457859982bc58
SulfiaM <114172240+SulfiaM@users.noreply.github.com>
2023-01-24 14:30:58 +0530

    initial commit

---
+++ task1/js/file1.js
@@ -0,0 +1,8 @@
+function myFunction() {
+    var a = document.getElementById("demo");
+    if (a.style.display === "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+}
