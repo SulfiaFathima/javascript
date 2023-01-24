f97e4457681be230b4082ad4e3a2e9481a343a1e
SulfiaFathima <123403518+SulfiaFathima@users.noreply.github.com>
2023-01-24 17:24:20 +0530

    Add files via upload

---
+++ webpage/js/file.js
@@ -0,0 +1,12 @@
+function log(txt) {
+    events.insertAdjacentHTML('beforeend', `<div>[${Math.floor(performance.now())}] ${txt}</div>`);
+}
+
+log('initial readyState:' + document.readyState);
+
+document.addEventListener('readystatechange', () => log('readyState:' + document.readyState));
+document.addEventListener('DOMContentLoaded', () => log('DOMContentLoaded'));
+
+window.onload = () => log('window onload');
+
+img.onload = () => log('img onload');
